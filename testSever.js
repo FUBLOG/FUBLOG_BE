1942687ae4494fb2652606fdf40440207fa98520
huynhmanh2003 <huynhvanmanh2003@gmail.com>
2024-05-20 16:55:01 +0700

    Crud V-1

---
+++ testSever.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require('./src/routes/postRoute');
+
+const app = express();
+app.use(express.json());
+const port = 4000;
+app.listen(port, () => {
+  console.log(`This app is running at port: ${port}`);
+});
+
+
+
+app.use('/API/posts', router);
+
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://sa:vanmanh2003@vanmanh123.bvzo5cd.mongodb.net/?retryWrites=true&w=majority&appName=vanmanh123')
+.then(()=> console.log('success'))
+
