66a2c43e7e69dc0fa406a88e1bf557087c9d91fd
huynhmanh2003 <136096602+huynhmanh2003@users.noreply.github.com>
2024-06-04 23:37:34 +0700

    Vanmanh (#9)
    * Crud V-1
    
    * ADD : add feature authentication/verifyEmail
    
    * Merge branch 'dev' of https://github.com/FUBLOG/FUBLOG_BE into dev
    
    * Config Post
    
    * ADD: authentication with accesstoken
    
    * post-ver3
    
    * Add: message feature and socket connect
    
    * done Tag and report
    
    * Done tag and report
    
    * done Vers4
    
    * Vanmanh (#6)
    
    * done Tag and report
    
    * Done tag and report
    
    * done Vers4
    
    ---------
    
    Co-authored-by: huynhmanh2003 <huynhvanmanh2003@gmail.com>
    
    * 1stver_Userinfo
    
    * Feat/authentication (#10)
    
    * 22
    
    * save
    
    * ADD : cors and notification feature
    
    * update
    
    * done
    
    * done
    
    ---------
    
    Co-authored-by: huynhmanh2003 <huynhvanmanh2003@gmail.com>
    Co-authored-by: josphanai0203 <Kaidophan37@gmail.com>
    Co-authored-by: josphanai0203 <115264959+josphanai0203@users.noreply.github.com>

--- src/routes/post/index.js
+++ src/routes/post/index.js
@@ -6,24 +6,27 @@ const asyncHandler = require("express-async-handler");
 const postcontroller = require("../../controller/postController");
 const uploader = require("../../config/cloudiany.config")
 
-router.get("/",postController.getallpost);
-router.post('/createpost',uploader.array('image'), asyncHandler(postcontroller.newpost));
-
-router.get("/:id", postController.getApost);
-router.get('/:id',asyncHandler(postcontroller.getApost));
-
-router.use(authentication);
-
-router.post("/", postController.newpost);
-router.get('/', asyncHandler(postcontroller.getallpost));
-
-router.patch('/:id', asyncHandler(postcontroller.updatePost));
-
-router.delete('/:id', asyncHandler(postcontroller.deletePost));
-
 router.get('/findPostByTag/:id', asyncHandler(postcontroller.searchPostsByTag));
 
 router.get('/findPostByUser/:id', asyncHandler(postcontroller.searchPostsByUserId));
+
+router.get('/getapost/:id',asyncHandler(postcontroller.getAPost));
+
+router.get('/getallpost', asyncHandler(postcontroller.getAllPost));
+
+
+
+// Authen
+router.use(authentication);
+router.post('/createpost',uploader.array('image'), asyncHandler(postcontroller.newPost));
+
+router.patch('/updateapost/:id', uploader.array('image'),asyncHandler(postcontroller.updatePost));
+
+router.delete('/deletepost/:id', asyncHandler(postcontroller.deletePost));
+
+
+
+
 
 
 module.exports = router;
