ce0ce43434742e537a4bf4c59183384145746ab1
josphanai0203 <115264959+josphanai0203@users.noreply.github.com>
2024-06-10 10:38:21 +0700

    Ai pv (#32)
    * Crud V-1
    
    * ADD : add feature authentication/verifyEmail
    
    * Merge branch 'dev' of https://github.com/FUBLOG/FUBLOG_BE into dev
    
    * Config Post
    
    * ADD: authentication with accesstoken
    
    * post-ver3
    
    * 22
    
    * Add: message feature and socket connect
    
    * Vanmanh (#6)
    
    * done Tag and report
    
    * Done tag and report
    
    * done Vers4
    
    ---------
    
    Co-authored-by: huynhmanh2003 <huynhvanmanh2003@gmail.com>
    
    * save
    
    * ADD : cors and notification feature
    
    * update
    
    * done
    
    * Feat/authentication (#10)
    
    * 22
    
    * save
    
    * ADD : cors and notification feature
    
    * update
    
    * done
    
    * FIX : Cors
    
    * save commit
    
    * Set Up Socket
    
    * FIX: fix cors socket
    
    * fix ssl cors
    
    * fix ssl cors 2
    
    * 1
    
    * update
    
    * done
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: huynhmanh2003 <huynhvanmanh2003@gmail.com>
    Co-authored-by: huynhmanh2003 <136096602+huynhmanh2003@users.noreply.github.com>

--- src/routes/report/index.js
+++ src/routes/report/index.js
@@ -20,5 +20,4 @@ router.patch('/updatereport/:id', asyncHandler(reportController.updateReport));
 
 router.delete('/deletereport/:id', asyncHandler(reportController.deleteReport));
 
-
 module.exports = router
