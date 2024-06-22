f5f8ce8f60e3d002e22da1980dd8a6a98c18ded6
josphanai0203 <Kaidophan37@gmail.com>
2024-06-22 15:15:16 +0700

    fix 4

--- src/app.js
+++ src/app.js
@@ -37,8 +37,8 @@ class App {
           maxAge: 1000 * 60 * 60 * 24,
           sameSite: "none",
           secure: "auto",
-          partitioned: true,
           httpOnly: true,
+          domain: "localhost",
         },
       })
     );
